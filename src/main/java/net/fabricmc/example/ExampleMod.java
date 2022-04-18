8723d9b5214c520a11030f7f1b902d4b45851b75
plusls <plusls@qq.com>
2022-04-19 00:55:30 +0800

    bug

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,6 +1,10 @@
 package net.fabricmc.example;
 
+import com.mojang.math.Matrix4f;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.level.BaseCommandBlock;
+import net.minecraft.world.level.BaseSpawner;
+import net.minecraft.world.level.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +19,12 @@ public class ExampleMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		Level level = null;
+		level.getDimensionLocation();
+		BaseCommandBlock baseCommandBlock = null;
+		baseCommandBlock.getDimensionLocation();
+		Matrix4f matrix4f = null;
+		matrix4f.getDimensionLocation();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/fabricmc/example/LevelCompatApi.java
@@ -0,0 +1,7 @@
+package net.fabricmc.example;
+
+import net.minecraft.resources.ResourceLocation;
+
+public interface LevelCompatApi {
+    ResourceLocation getDimensionLocation();
+}
