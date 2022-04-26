1e04fa95b86c36b31d9144cea4fef84d3be31b0c
plulsls <plusls@qq.com>
2022-04-26 09:19:49 +0800

    bug3

---
+++ src/main/java/net/fabricmc/example/mixin/AccessorEntity.java
@@ -0,0 +1,11 @@
+package net.fabricmc.example.mixin;
+
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Entity.class)
+public interface AccessorEntity {
+    @Accessor
+    Entity getVehicle();
+}
